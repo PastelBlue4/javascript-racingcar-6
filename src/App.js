0c7fcf6d279adac1740cdc20bbb224019c57417b
Romuru <pastelblue0721@gmail.com>
2023-11-01 23:22:39 +0900

    feat (3):  유저의 입력이 올바른 규칙인지 검증

--- src/App.js
+++ src/App.js
@@ -11,7 +11,24 @@ class App {
       "게임을 시도할 횟수를 입력해 주세요.\n"
     );
 
-    const playArray = playerBeforeDivide.split(",");
+    const playArray = playerBeforeDivide.split(",").trim();
+
+    playArray.forEach((player) => {
+      if (player === "") {
+        throw new Error(
+          "[ERROR] 공백없는 5자 이하의 자동차 이름만 가능합니다."
+        );
+      }
+      if (player.length > 5) {
+        throw new Error(
+          "[ERROR] 공백없는 5자 이하의 자동차 이름만 가능합니다."
+        );
+      }
+    });
+
+    if (isNaN(gameRound) || gameRound >= 1) {
+      throw new Error("[ERROR] 올바른 숫자를 입력해주세요.");
+    }
   }
 
   async play() {}
